38ba1f6cf385dd6e1d7df398d209aaba00e48219
peter muyambango <muyambangopeter@gmail.com>
2024-02-13 23:31:34 +0200

    minors changes to signup function

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -20,7 +20,7 @@ export const login = (req: Request, res: Response) => {
   const { email, password } = req.body
   User.findOne({ email }).then(user => {
     if (!user) {
-      res.send('No user was found. Create account')
+      res.json({ message: 'No user was found. Create account' })
     } else {
       //if a user was found, we check if the password they entered == the one saved in the db
       bcrypt.compare(password, user.password, (err, result) => {
@@ -53,9 +53,9 @@ export const signup = (req: Request, res: Response) => {
           res
             .cookie('jwt', token, { maxAge: maxAge * 1000 })
             .status(201)
-            .json({ message: 'Account has been created successfully!' })
+            .json(createdUser)
         })
-        .catch(error => res.json({ error }))
+        .catch(error => res.json({ message: error }))
     }
   })
 }
